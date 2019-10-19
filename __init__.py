b0fd8a1d40337af170e69f8f19eb3e66c2cb30c6
Robert Sefer <rsefer@gmail.com>
2019-10-19 12:17:49 -0500

    Unhardcoded host and port

--- __init__.py
+++ __init__.py
@@ -8,9 +8,11 @@ def setup(hass, config):
 
 	def telnet_commands_service(call):
 		_LOGGER.info('Received data', call.data)
+		host = config['telnet_commands']['host'] or '192.168.0.2'
+		port = config['telnet_commands']['port'] or 31339
 		command = call.data.get('command', 'SETCH')
 		value = call.data.get('value', '0612')
-		telnet = telnetlib.Telnet('192.168.0.12', 31339)
+		telnet = telnetlib.Telnet(host, port)
 		full_command = command + " " + value
 		telnet.write(full_command.encode("ASCII") + b"\r")
 		response = telnet.read_until(b"\r", timeout=0.5)
